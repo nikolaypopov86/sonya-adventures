5f1578d3755125f626ddf7c0e72ba676b72db04a
Nikolay Popov-Akulov <nikolaypopov.perm@gmail.com>
2025-09-05 19:19:23 +0500

    исправил неправильные пути в файлах .tmx

--- data/tilesets/coin.tsx
+++ data/tilesets/coin.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.11" tiledversion="1.11.2" name="coin" tilewidth="16" tileheight="16" tilecount="12" columns="12">
- <image source="../../../brackeys_platformer_assets/sprites/coin.png" width="192" height="16"/>
+ <image source="./coins/coin_0.png" width="192" height="16"/>
 </tileset>
--- data/tilesets/fruit.tsx
+++ data/tilesets/fruit.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.11" tiledversion="1.11.2" name="fruit" tilewidth="16" tileheight="16" tilecount="16" columns="4">
- <image source="../../../brackeys_platformer_assets/sprites/fruit.png" width="64" height="64"/>
+ <image source="./fruit.png" width="64" height="64"/>
 </tileset>
