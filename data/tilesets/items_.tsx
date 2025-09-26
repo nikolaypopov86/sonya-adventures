56d63632b7f979d98c1a6bfe5bbca8594335e052
nikolaypopov86 <nikolaypopov.perm@gmail.com>
2025-09-27 00:03:11 +0500

    добавил 20 уровень

---
+++ data/tilesets/items_.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="items_" tilewidth="16" tileheight="16" tilecount="12" columns="4">
+ <image source="items_.png" width="64" height="48"/>
+</tileset>
