95d49186b58e2a8c60776f67fc0c4f624e6e41cd
nikolaypopov86 <nikolaypopov.perm@gmail.com>
2025-09-26 21:04:08 +0500

    правки в скриптах

--- compress.linux.sh
+++ compress.linux.sh
@@ -1,5 +1,5 @@
 mkdir -p release
 rm ./release/* 2> /dev/null
 cp ./build/app.bin ./app.bin
-tar --exclude='./data/app.tiled-*' -cjvf release/sonya-adventures.bz2 ./data ./app.bin
+tar --exclude='./data/app.tiled-*' -cjvf release/sonya-adventures.linux-x86_64.bz2 ./data ./app.bin
 rm ./app.bin
--- compress.windows.sh
+++ compress.windows.sh
@@ -5,5 +5,5 @@ rm -rf ./build-win/data/*
 rm ./release-win/* 2> /dev/null
 cp -r data ./build-win/
 rm ./build-win/data/app.tiled-project ./build-win/data/app.tiled-session
-zip -r ./build-win/*
+zip -r ./release-win/sonya-adventures.win-x86_64.zip ./build-win/*
 #rm ./app.bin
